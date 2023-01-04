267ae279d9c8c9a3280d50e736661b414ee1ad88
Natnael Gebremichael <natnael92460@gmail.com>
2023-01-04 16:07:36 +1300

    Create Task Screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
+import Home from "./screens/Home";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import CreateTask from "./screens/CreateTask";
+import {
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tasks"
+          component={Home}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <TouchableWithoutFeedback
+                onPress={() => navigation.navigate("CreateTask")}
+              >
+                <Ionicons
+                  name={"ios-add"}
+                  size={34}
+                  color={"#0080ff"}
+                  style={{ marginRight: 25 }}
+                />
+              </TouchableWithoutFeedback>
+            ),
+          })}
+        />
+        <Stack.Screen name="CreateTask" component={CreateTask} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
