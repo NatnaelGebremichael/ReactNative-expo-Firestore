123f9b47aaaa3ee94b2593374c2f31e347804c88
Natnael Gebremichael <natnael92460@gmail.com>
2023-01-04 13:36:23 +1300

    Daynamic CheckBox Icon, Tasks(note)

--- component/Checkbox.tsx
+++ component/Checkbox.tsx
@@ -1,7 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
-const Checkbox: FC = () {
-    return (
-        
-    )
-}
+type Props = {
+  value: boolean;
+};
+
+const Checkbox: FC<Props> = ({ value }: Props) => {
+  const [isChecked, setChecked] = useState<Boolean>(value);
+
+  return (
+    <TouchableOpacity onPress={() => setChecked(!isChecked)}>
+      {isChecked ? (
+        <Feather name={"check-square"} size={24} color={"black"} />
+      ) : (
+        <Feather name={"square"} size={24} color={"black"} />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default Checkbox;
--- component/tasks.tsx
+++ component/tasks.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Checkbox from "./Checkbox";
 
 const Tasks = () => {
   return (
     <View>
-      <View>
+      <View style={style.taskItem}>
         <Text>Lift Weights</Text>
+        <Checkbox value={true} />
       </View>
-      <View>
+      <View style={style.taskItem}>
         <Text>Eat Well</Text>
+        <Checkbox value={false} />
       </View>
     </View>
   );
 };
 
+const style = StyleSheet.create({
+  taskItem: {
+    flexDirection: "row",
+  },
+});
 export default Tasks;
