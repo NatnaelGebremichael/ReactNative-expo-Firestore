4214c14654b9aaa8ec05e3d9eb7aac196ad7f230
Natnael Gebremichael <natnael92460@gmail.com>
2023-01-04 13:22:10 +1300

    expo vector icons

---
+++ component/tasks.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Tasks = () => {
+  return (
+    <View>
+      <View>
+        <Text>Lift Weights</Text>
+      </View>
+      <View>
+        <Text>Eat Well</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Tasks;
